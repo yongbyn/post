37887d736240d4ee2ffcaa6cbfd2529056f69ae8
YongBin Kim <yongbyn@naver.com>
2024-10-21 22:20:19 +0900

    feat. repository 작성

---
+++ src/main/java/com/example/post/post/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.example.post.post.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.post.domain.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
